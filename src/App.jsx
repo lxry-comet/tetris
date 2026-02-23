29bcd0becbef5f3cdd69a30c8deb85427c7f419d
lxry-comet <durdentagirowich@gmail.com>
2026-02-23 22:59:47 +0100

    create tetris

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useState } from 'react'
 import './App.css'
+import Tetris from 'react-tetris'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+	return (
+		<div className='gameWindow'>
+			<h1>Tetris</h1>
+			<Tetris
+				keyboardControls={{
+					down: 'MOVE_DOWN',
+					left: 'MOVE_LEFT',
+					right: 'MOVE_RIGHT',
+					space: 'HARD_DROP',
+					z: 'FLIP_COUNTERCLOCKWISE',
+					x: 'FLIP_CLOCKWISE',
+					up: 'FLIP_CLOCKWISE',
+					p: 'TOGGLE_PAUSE',
+					c: 'HOLD',
+					shift: 'HOLD'
+				}}
+			>
+				{({
+					HeldPiece,
+					Gameboard,
+					PieceQueue,
+					points,
+					linesCleared,
+					state,
+					controller
+				}) => (
+					<div>
+						<div className='gameboardtext'>
+							<p>
+								Points:
+								<br /> {points}
+							</p>
+							<p>
+								Lines Cleared:
+								<br /> {linesCleared}
+							</p>
+						</div>
+						<div className='gameboard'>
+							<div
+								style={{
+									display: 'flex',
+									flexDirection: 'column',
+									justifyContent: 'flex-end'
+								}}
+							>
+								<HeldPiece />
+							</div>
+							<div>
+								<Gameboard />
+							</div>
+							<div
+								style={{
+									display: 'flex',
+									flexDirection: 'column',
+									justifyContent: 'flex-end'
+								}}
+							>
+								<PieceQueue />
+							</div>
+						</div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+						{(state === 'LOST' || state === 'GAME_OVER') && (
+							<div className='game-over-overlay'>
+								<h2>Game Over</h2>
+								<button onClick={controller.restart}>New game</button>
+							</div>
+						)}
+					</div>
+				)}
+			</Tetris>
+		</div>
+	)
 }
-
-export default App
